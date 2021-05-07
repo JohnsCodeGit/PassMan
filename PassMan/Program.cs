4b35e88a80204589c4b5380917c3bd4e9abb1b11
John Melton <56171427+JohnsCodeGit@users.noreply.github.com>
2021-05-07 09:46:41 -0400

    Updated Encryption

--- PassMan/Program.cs
+++ PassMan/Program.cs
@@ -5,23 +5,48 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Security;
 using System.Security.Cryptography;
+using BCrypt.Net;
 
 namespace PassMan
 {
     class Program
     {
 
+
+
         public static void Main()
         {
             List<string> passwordList = new List<string>();
-            passwordList.Add("helloo");
-            passwordList.Add("?q?$?b?x]??j??=s1????♀?????%↓g<?##?????↔|z?n¶???♀FcG\\.\\:??os???C");
-            passwordList.Add("123");
-            GetCredentials(passwordList);
+
+            LoginMenu(passwordList);
+        }
+
+        private static void LoginMenu(List<string> list)
+        {
+            Console.Clear();
+            string dash = new string('-', 14);
+            Console.WriteLine(dash);
+            Console.WriteLine("PassMan Login");
+            Console.WriteLine(dash);
+
+            Console.WriteLine("1. Add Account");
+            Console.WriteLine("2. Sign-in\n");
+
+            Console.Write("Choice: ");
+
+            string choice = Console.ReadLine();
+
+            if(choice.Equals("1")){
+                CreateAccount(list);
+            }
+            else if(choice.Equals("2")){
+                SignIn(list);
+            }
+
         }
 
          //by MSDN http://msdn.microsoft.com/en-us/library/844skk0h(v=vs.71).aspx
-         public static SecureString UseRegex(SecureString strIn)
+        public static SecureString UseRegex(SecureString strIn)
         {
             // Replace invalid characters with empty strings.
             string theString = new NetworkCredential("", strIn).Password;
@@ -31,23 +56,44 @@ namespace PassMan
             
         }
 
-        private static void GetCredentials(List<string> list)
+        private static void CreateAccount(List<string> list)
         {
+            Console.Clear();
+            NetworkCredential cred = new NetworkCredential();
+
+            Console.Write("Create Username: ");
+            cred.UserName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.Write("Create Password: ");
+            cred.Password = Console.ReadLine();
+            
+            var encryptedPassword = EncryptPasswordToString(cred);
+
+            list.Add(encryptedPassword);
+
+            LoginMenu(list);
+        }
+
+        private static void SignIn(List<string> list)
+        {
+            Console.Clear();
             bool isValid = false;
             
             char[] charsToTrim = {' ', '\''};
 
             do{
-                string user_name;
-                NetworkCredential credentials;
+               
+                NetworkCredential credentials = new NetworkCredential();
                
                 Console.WriteLine("Enter username: ");
-                user_name = Console.ReadLine().Trim(charsToTrim);
+                credentials.UserName = Console.ReadLine().Trim(charsToTrim);
 
                 Console.WriteLine("Enter password: ");
-                credentials = new NetworkCredential(user_name, Console.ReadLine());
+                credentials.Password = Console.ReadLine();
+                // credentials.Password = EncryptPasswordToString(credentials);
  
-                if(TestPassword(credentials, list))
+                if(Login(credentials, list))
                 {
                     Console.WriteLine("\nPassword accepted\n");
                     
@@ -64,30 +110,49 @@ namespace PassMan
           
         }
 
-        private static bool TestPassword(NetworkCredential cred, List<string> list){
+        private static string EncryptPasswordToString(NetworkCredential cred){ 
 
-            SHA512 sHA = new SHA512Managed();
-            byte[] compArr = sHA.ComputeHash(ASCIIEncoding.ASCII.GetBytes(cred.Password));
+            var salt =  BCrypt.Net.BCrypt.GenerateSalt();
+
+            return BCrypt.Net.BCrypt.HashPassword(cred.Password, salt);
+        }
+
+        private static bool Login(NetworkCredential cred, List<string> list){
+
+            // var compArr = EncryptPasswordToByteArray(cred);
 
             for(int i = 0; i < list.Count; i++)
             {  
-                byte[] item = ASCIIEncoding.ASCII.GetBytes(list[i]);
-                bool isValid = false;
-
-                for(int j = 0; j < item.Length; j++){
-
-                    if(item[j] == compArr[j]){
-                        isValid = true;
-                    }
-                    else{
-                        isValid = false;
-                    }                  
-                }
-
-                if(isValid)
-                    return true;      
+                if(BCrypt.Net.BCrypt.Verify(cred.Password, list[i]))
+                {
+                    return true;
+                }           
             }
             return false;
         }
     }
 }
+
+        // private static byte[] EncryptPasswordToByteArray(NetworkCredential cred){
+            
+
+        //     return sHA.ComputeHash(ASCIIEncoding.ASCII.GetBytes(cred.Password));
+        // }
+
+            //     byte[] item = ASCIIEncoding.ASCII.GetBytes(list[i]);
+            //     bool isValid = false;
+
+            //     Console.WriteLine("{0}, {1}", item.Length, compArr.Length);
+                
+            //     for(int j = 0; j < item.Length; j++){
+
+            //         if(item.Length == compArr.Length && item[j] == compArr[j]){
+            //             isValid = true;
+            //         }
+            //         else{
+            //             isValid = false;
+            //         }                  
+            //     }
+
+            //     if(isValid)
+            //         return true;    
